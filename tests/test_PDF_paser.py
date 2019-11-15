49fa443c6e6e91e00e585a6dd2dad23ef496aa7a
Bill Chen <billchenxi@gmail.com>
2019-11-15 08:38:11 -0800

    Add items in gitignore, temp test

--- tests/test_PDF_paser.py
+++ tests/test_PDF_paser.py
@@ -1,3 +1,11 @@
 import unittest
+non_info_list = ["Art.", "Art", "Article", "Sec.", "Sect.", "Section", 
+                    "Sec", "Part", "Exhibit"]
 
-from FeatureGeneration.PDF_paser import Features_Generation
+[first_name, middle_name, period, last_name] = ['Ed', 'Jh', '.', 'Thomasons']
+
+if (first_name.istitle() or first_name[0].isupper()) and \
+    (last_name.istitle() or last_name[0].isupper()) and \
+    (len(middle_name)==1) and period=='.' and \
+    first_name not in non_info_list:
+    print("Yeah")
